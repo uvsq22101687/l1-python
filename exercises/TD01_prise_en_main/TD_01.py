8a0a68de3fa60852badcf1038fd7fbcafd378ee3
Ludivine Mendy <ludivine.mendy@ens.uvsq>
2022-09-28 16:18:22 +0200

    update2

--- exercises/TD01_prise_en_main/TD_01.py
+++ exercises/TD01_prise_en_main/TD_01.py
@@ -17,5 +17,51 @@ print("a vaut", a, "et b vaut", b)
 a = "hello world"
 print(a)
 a
-a = 5
-print(type(a))
+type(5)
+a = 5.0
+type(5.0)
+type("5")
+type(-5)
+type((5/2))
+type(5//2)
+type(5==2)
+type(5=2)
+#list
+type([5, 2])
+type({5, 2})
+x = 6
+type(x)
+v = 149
+18 //
+3 * (10 / 3)
+3 * (10 // 3)
+(3 * 10) // 3
+l = 8
+l%2==0
+2**256
+print(2**256, "le nombre de chiffres est", len(str(2**256)))
+print(2**256, "le dernier chiffre est",)
+a = 0.2 + 0.4
+res = (a==0.6) or (a>0.6)
+print(res)
+x = 0.5
+x * (2**30)
+int(x)
+x / 2**30
+x = 0.1
+x * 2**30
+int(x)
+x * 2**30
+a = "hello" 
+b = "world"
+a + b 
+b + a 
+a * 3
+(a + b) * 2
+a . upper()
+a > b
+a = 32
+a * 2 
+int()
+s = input("tape quelque chose au clavier")
+
